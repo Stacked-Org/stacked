22ae5576766ac4d4b0202bf9b3e561aacecca6cc
Dane Mackier <dane.mackier@outlook.com>
2020-04-23 15:01:11 +0200

    Added get key to navigator

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:new_architecture/app/locator.dart';
 import 'package:new_architecture/ui/stream_view/stream_counter_view.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      navigatorKey: Get.key,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
