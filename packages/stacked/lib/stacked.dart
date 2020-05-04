30e6bbeb1203db866f245ff3251ab370688db753
creativecreatorormaybenot <19204050+creativecreatorormaybenot@users.noreply.github.com>
2020-05-04 21:07:44 +0000

    Reorder exports

--- packages/stacked/lib/stacked.dart
+++ packages/stacked/lib/stacked.dart
@@ -1,7 +1,7 @@
 library stacked;
 
 export 'src/base_view_models.dart';
-export 'src/view_model_widget.dart';
 export 'src/reactive_service_mixin.dart';
 export 'src/view_model_builder.dart';
 export 'src/view_model_builder_widget.dart';
+export 'src/view_model_widget.dart';
