9daf4bd130b474ecb2a5b407451120b638eab13f
Dane Mackier <dane.mackier@outlook.com>
2020-07-08 07:39:44 +0200

    Exposes index tracking viewmodel

--- packages/stacked/lib/stacked.dart
+++ packages/stacked/lib/stacked.dart
@@ -1,6 +1,7 @@
 library stacked;
 
 export 'src/base_view_models.dart';
+export 'src/index_tracking_viewmodel.dart';
 export 'src/reactive_service_mixin.dart';
 export 'src/view_model_builder.dart';
 export 'src/view_model_builder_widget.dart';
