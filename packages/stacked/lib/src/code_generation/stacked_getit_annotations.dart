bd45ef30aa6003ce97b0e21f3fd20acf8d2ef3b6
Dane Mackier <dane.mackier@outlook.com>
2021-02-21 10:08:11 +0200

    Removes the foundation import for required params

--- packages/stacked/lib/src/code_generation/stacked_getit_annotations.dart
+++ packages/stacked/lib/src/code_generation/stacked_getit_annotations.dart
@@ -1,32 +1,29 @@
-import 'package:flutter/foundation.dart';
-
 /// The class to describe a service registration on the get_it locator
 class DependencyRegistration {
   /// The type of the service to register
   final Type service;
 
-  const DependencyRegistration({@required this.service});
+  const DependencyRegistration({this.service});
 }
 
 /// Registers the type passed in as a singleton instance in the get_it locator
 class Singleton extends DependencyRegistration {
-  const Singleton({@required Type service}) : super(service: service);
+  const Singleton({Type service}) : super(service: service);
 }
 
 /// Registers the type passed in as a LazySingleton instance in the get_it locator
 class LazySingleton extends DependencyRegistration {
-  const LazySingleton({@required Type service}) : super(service: service);
+  const LazySingleton({Type service}) : super(service: service);
 }
 
 /// Registers the type passed in as a Factory in the get_it locator
 class Factory extends DependencyRegistration {
-  const Factory({@required Type service}) : super(service: service);
+  const Factory({Type service}) : super(service: service);
 }
 
 /// Registers the type passed in to be presolved using the function passed in
 class Presolve extends DependencyRegistration {
   final Future Function() presolveUsing;
 
-  const Presolve({@required Type service, @required this.presolveUsing})
-      : super(service: service);
+  const Presolve({Type service, this.presolveUsing}) : super(service: service);
 }
