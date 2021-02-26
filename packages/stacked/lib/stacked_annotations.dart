0e7b86cfb1d38e25b5bca82e437f41812b8df699
Dane Mackier <dane.mackier@outlook.com>
2021-02-26 13:56:35 +0200

    [stacked] - Adds forms annotation related classes

--- packages/stacked/lib/stacked_annotations.dart
+++ packages/stacked/lib/stacked_annotations.dart
@@ -1,2 +1,3 @@
 export 'src/code_generation/stacked_router_annotations.dart';
 export 'src/code_generation/stacked_getit_annotations.dart';
+export 'src/code_generation/forms/stacked_form_annotations.dart';
