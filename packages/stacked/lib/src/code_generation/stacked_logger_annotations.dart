406ee82ffec24aea857134e694f277f24b392c3a
Arnab Banerjee <banerjeearnab38@gmail.com>
2021-06-18 01:40:25 +0530

    Update stacked_logger_annotations.dart

--- packages/stacked/lib/src/code_generation/stacked_logger_annotations.dart
+++ packages/stacked/lib/src/code_generation/stacked_logger_annotations.dart
@@ -1,6 +1,12 @@
+import 'package:logger/logger.dart';
+
 /// Defined the config for the logger in the app
 class StackedLogger {
   final String logHelperName;
+  final List<Type> multiLogger;
 
-  const StackedLogger({this.logHelperName = 'getLogger'});
+  const StackedLogger({
+    this.multiLogger = const [],
+    this.logHelperName = 'getLogger',
+  });
 }
