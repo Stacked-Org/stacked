0311c54ed1a9504fb473b1a1dfdd05b47d57e1d5
Arnab Banerjee <banerjeearnab38@gmail.com>
2021-06-18 12:09:15 +0530

    added small documentation

--- packages/stacked/lib/src/code_generation/stacked_logger_annotations.dart
+++ packages/stacked/lib/src/code_generation/stacked_logger_annotations.dart
@@ -1,6 +1,10 @@
 /// Defined the config for the logger in the app
 class StackedLogger {
   final String logHelperName;
+
+  /// Pass loggerOutputs types here.[logger: StackedLogger(loggerOutputs: [CrashlyticsOutput])]
+  ///
+  /// loggerOutputs must extends [LogOutput]
   final List<Type> loggerOutputs;
 
   const StackedLogger({
