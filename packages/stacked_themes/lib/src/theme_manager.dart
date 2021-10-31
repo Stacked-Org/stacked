7647417dc1acbeba349b7b18a01f1e535e165fee
Zain Ul Hassan <zainulhassan4330@gmail.com>
2021-10-31 21:20:48 +0500

    Optimize Imports

--- packages/stacked_themes/lib/src/theme_manager.dart
+++ packages/stacked_themes/lib/src/theme_manager.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
