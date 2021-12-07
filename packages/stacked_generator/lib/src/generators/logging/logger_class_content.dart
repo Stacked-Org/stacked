2c4877a634a8c51514ff9df80fbbeed97220a8fc
Espéra AWO <esperaking81@gmail.com>
2021-12-07 21:30:27 +0100

    Update logger_class_content.dart
    add // ignore_for_file: avoid_print to respect flutter_lints rules.

--- packages/stacked_generator/lib/src/generators/logging/logger_class_content.dart
+++ packages/stacked_generator/lib/src/generators/logging/logger_class_content.dart
@@ -4,6 +4,8 @@ const String MultipleLoggerOutput = 'MultiLoggerList';
 const String DisableConsoleOutputInRelease = 'MultiLoggerList';
 
 const String loggerClassContent = '''
+// ignore_for_file: avoid_print
+
 /// Maybe this should be generated for the user as well?
 ///
 /// import 'package:customer_app/services/stackdriver/stackdriver_service.dart';
