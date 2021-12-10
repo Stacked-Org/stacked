d8db7bdffad90362222282652cba25b47b9858ba
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2021-12-10 10:35:43 +0000

    :bug: Omitted logs in release mode

--- packages/stacked_generator/lib/src/generators/logging/logger_class_content.dart
+++ packages/stacked_generator/lib/src/generators/logging/logger_class_content.dart
@@ -122,6 +122,24 @@ class MultipleLoggerOutput extends LogOutput {
   }
 }
 
+/// Prints all logs with `level >= Logger.level` while in development mode (eg
+/// when `assert`s are evaluated, Flutter calls this debug mode).
+///
+/// In release mode ALL logs are omitted.
+class StackedFilter extends LogFilter {
+  @override
+  bool shouldLog(LogEvent event) {
+    var shouldLog = false;
+    assert(() {
+      if (event.level.index >= level!.index) {
+        shouldLog = true;
+      }
+      return true;
+    }());
+    return shouldLog;
+  }
+}
+
 Logger $LogHelperNameKey(
   String className, {
   bool printCallingFunctionName = true,
@@ -130,6 +148,7 @@ Logger $LogHelperNameKey(
   String? showOnlyClass,
 }) {
   return Logger(
+    filter: StackedFilter(),
     printer: SimpleLogPrinter(
       className,
       printCallingFunctionName: printCallingFunctionName,
