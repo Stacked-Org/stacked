fa2aa3d5dead16cc1af0bf694f7eb242937faaac
Dane Mackier <dane.mackier@outlook.com>
2022-02-02 16:42:30 +0200

    Creates the stacked-cli and adds first template file

---
+++ packages/stacked_cli/bin/stacked_cli.dart
@@ -0,0 +1,5 @@
+import 'package:stacked_cli/stacked_cli.dart' as stacked_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${stacked_cli.calculate()}!');
+}
---
+++ packages/stacked_cli/test/stacked_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:stacked_cli/stacked_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
