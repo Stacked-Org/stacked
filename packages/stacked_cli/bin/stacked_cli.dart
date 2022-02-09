7e11d8fc3c0518d45ea7c02fd9f63138d09fa468
Dane Mackier <dane.mackier@outlook.com>
2022-02-09 15:20:43 +0200

    [cli] Adds compile command

--- packages/stacked_cli/bin/stacked_cli.dart
+++ packages/stacked_cli/bin/stacked_cli.dart
@@ -1,30 +1,27 @@
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
+import 'package:stacked_cli/src/commands/compile/compile_command.dart';
 import 'package:stacked_cli/src/commands/create/create_command.dart';
 import 'package:stacked_cli/src/locator.dart';
 import 'package:stacked_cli/src/message_constants.dart';
 import 'package:stacked_cli/src/services/file_service.dart';
-import 'package:stacked_cli/src/services/template_service.dart';
 
 Future<void> main(List<String> arguments) async {
   // Is this even right? I'm fine with this for now. Will look for better way to do it.
   setupLocator();
 
-  final templateService = locator<TemplateService>();
-  templateService.compileTemplateInformation();
+  if (arguments.contains('create')) {
+    // Check if we are at the root of the project. If not, exit gracefully
+    if (!(await locator<FileService>().isProjectRoot())) {
+      print(kInvalidRootDirectory);
+      exit(2);
+    }
 
-  return;
-
-  // Check if we are at the root of the project. If not, exit gracefully
-  if (!(await locator<FileService>().isProjectRoot())) {
-    print(kInvalidRootDirectory);
-    exit(2);
-  }
-
-  if (!(await locator<FileService>().isStakedApplication())) {
-    print(kInvalidStackedStructure);
-    exit(2);
+    if (!(await locator<FileService>().isStakedApplication())) {
+      print(kInvalidStackedStructure);
+      exit(2);
+    }
   }
 
   // TODO: When adding the command add accepted values to ensure we can never pass in random values.
@@ -33,5 +30,6 @@ Future<void> main(List<String> arguments) async {
     'A command line interface for building and scaffolding stacked apps',
   )
     ..addCommand(CreateCommand())
+    ..addCommand(CompileCommand())
     ..run(arguments);
 }
