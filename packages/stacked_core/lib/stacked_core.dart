3ba7e62df8592fc98fdd0549b6f2405be5b362d1
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2022-05-02 16:43:19 +0300

    - Create stacked_core package - Move annotation code to stacked_core

---
+++ packages/stacked_core/lib/stacked_core.dart
@@ -0,0 +1,13 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library stacked_core;
+
+export 'src/code_generation/stacked_router_annotations.dart';
+export 'src/code_generation/stacked_locator_annotations.dart';
+export 'src/code_generation/stacked_logger_annotations.dart';
+export 'src/code_generation/forms/stacked_form_annotations.dart';
+export 'src/code_generation/stacked_app.dart';
+export 'src/code_generation/environment_filter.dart';
+
+// TODO: Export any libraries intended for clients of this package.
--- packages/stacked_services/lib/src/navigation_service.dart
+++ packages/stacked_services/lib/src/navigation_service.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class NavigationTransition {
