304730f760c7f1b3d4502c07424b12bad7d1af7f
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2022-05-03 10:38:40 +0300

    update stacked example

--- packages/stacked/example/lib/ui/nonreactive/nonreactive_viewmodel.dart
+++ packages/stacked/example/lib/ui/nonreactive/nonreactive_viewmodel.dart
@@ -1,10 +1,19 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+import '../../app/app.locator.dart';
+import '../../app/app.router.dart';
 
 class NonReactiveViewModel extends BaseViewModel {
   String title = 'This should not change';
+  final _navigationService = exampleLocator<NavigationService>();
 
   void updateTitle() {
     title += '. This has changed';
     notifyListeners();
+  }
+
+  void navigateToNewView() {
+    _navigationService.navigateTo(Routes.streamCounterView);
   }
 }
