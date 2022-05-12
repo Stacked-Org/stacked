dde0708b0e8393d114bf1d128c4ba3f0be7cc42d
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2022-05-12 18:17:47 +0300

    revert FactoryParameter annotation to FactoryParam

--- packages/stacked_generator/lib/src/generators/getit/stacked_locator_parameter_resolver.dart
+++ packages/stacked_generator/lib/src/generators/getit/stacked_locator_parameter_resolver.dart
@@ -1,11 +1,12 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:source_gen/source_gen.dart';
+import 'package:stacked_core/stacked_core.dart';
 import 'package:stacked_generator/import_resolver.dart';
 import 'package:stacked_generator/utils.dart';
 
 import 'dependency_config/factory_param_dependency.dart';
 
-const _factoryParamChecker = TypeChecker.fromRuntime(FactoryParameter);
+const _factoryParamChecker = TypeChecker.fromRuntime(FactoryParam);
 
 class DependencyParameterResolver {
   final ImportResolver _importResolver;
