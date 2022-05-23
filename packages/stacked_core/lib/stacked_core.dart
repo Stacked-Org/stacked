be47ba102e0104fabbf6c3feec579117270a8d82
Eimen2018 <nur.amuni26@gmail.com>
2022-05-23 16:26:38 +0300

    remove duplicated exports

--- packages/stacked_core/lib/stacked_core.dart
+++ packages/stacked_core/lib/stacked_core.dart
@@ -4,7 +4,6 @@
 library stacked_core;
 
 export 'src/backward_compatibility/flutter_v2_compatibility.dart';
-export 'src/backward_compatibility/flutter_v2_compatibility.dart';
 export 'src/code_generation/environment_filter.dart';
 export 'src/code_generation/forms/stacked_form_annotations.dart';
 export 'src/code_generation/locator/stacked_locator.dart';
