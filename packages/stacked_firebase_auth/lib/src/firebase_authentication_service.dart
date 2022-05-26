41e5b120a9e966dafb80443d87f482b83befa8c1
Dane Mackier <dane.mackier@outlook.com>
2022-05-26 09:17:52 +0200

    Removes log for credential

--- packages/stacked_firebase_auth/lib/src/firebase_authentication_service.dart
+++ packages/stacked_firebase_auth/lib/src/firebase_authentication_service.dart
@@ -184,7 +184,7 @@ class FirebaseAuthenticationService {
       final familyName = appleIdCredential.familyName;
       final hasFamilyName = familyName != null;
 
-      print('Apple Sign in complete: ${appleIdCredential.toString()}');
+      // print('Apple Sign in complete: ${appleIdCredential.toString()}');
 
       await appleCredential.user?.updateDisplayName(
           '${hasGivenName ? givenName : ''}${hasFamilyName ? ' $familyName' : ''}');
