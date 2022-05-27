b173f41fe298510647bbd62ce17030d376b6b62f
Fernando Ferrara <ferrarafer@gmail.com>
2022-05-26 23:36:20 -0300

    Improve ConfigService
    - Use conventions from Filledstacks team
    - Add replaceCustomPaths method
    - Add _getImportPath method
    - Add _getRelativePathToHelpersAndMocks method

--- packages/stacked_tools/lib/src/services/config_service.dart
+++ packages/stacked_tools/lib/src/services/config_service.dart
@@ -13,36 +13,59 @@ class ConfigService {
   final _log = locator<ColorizedLogService>();
   final _fileService = locator<FileService>();
 
-  Config _config = Config();
+  /// Default config map used to compare and replace with custom values
+  final Map<String, dynamic> _defaultConfig = Config().toJson();
 
-  /// Path of the file where StackedApp is setup.
-  String get stackedAppPath => _config.stackedAppPath;
+  /// Custom config used to store custom values read from file
+  Config _customConfig = Config();
 
-  /// Path where views and viewmodels will be genereated.
-  String get viewPath => _config.viewPath;
+  bool _hasCustomConfig = false;
 
-  /// Path where viewmodels unit tests will be genereated.
-  String get viewTestsPath => _config.viewTestsPath;
+  bool get hasCustomConfig => _hasCustomConfig;
 
-  /// Path where services will be genereated.
-  String get servicePath => _config.servicePath;
+  /// Relative services path for import statements.
+  String get serviceImportPath => _getImportPath(_customConfig.servicesPath);
 
-  /// Path where services unit tests will be genereated.
-  String get serviceTestsPath => _config.serviceTestsPath;
+  /// Relative path where services will be genereated.
+  String get servicePath => _customConfig.servicesPath;
 
-  /// Path for test helper file that contains the register functions for unit
-  /// test setup.
-  String get testHelpersPath => _config.testHelpersPath;
+  /// Relative import path related to services of test helpers and mock services.
+  String get serviceTestHelpersImportPath => _getRelativePathToHelpersAndMocks(
+        _customConfig.testServicesPath,
+      );
 
-  /// Path for mock declarations.
-  String get testMocksPath => _config.testMocksPath;
+  /// Relative path of the file where StackedApp is setup.
+  String get stackedAppPath => _customConfig.stackedAppPath;
+
+  /// Relative path for test helper file that contains the register functions
+  /// for unit test setup.
+  String get testHelpersPath => _customConfig.testHelpersPath;
+
+  /// Relative path where services unit tests will be genereated.
+  String get testServicesPath => _customConfig.testServicesPath;
+
+  /// Relative path where viewmodels unit tests will be genereated.
+  String get testViewsPath => _customConfig.testViewsPath;
+
+  /// Relative views path for import statements.
+  String get viewImportPath => _getImportPath(_customConfig.viewsPath);
+
+  /// Relative path where views and viewmodels will be genereated.
+  String get viewPath => _customConfig.viewsPath;
+
+  /// Relative import path related to viewmodels of test helpers and mock services.
+  String get viewTestHelpersImportPath => _getRelativePathToHelpersAndMocks(
+        _customConfig.testViewsPath,
+      );
 
   /// Check if configuration file at [path] exists.
   Future<bool> isConfigFileAvailable({String path = kConfigFilePath}) async {
     return await _fileService.fileExists(filePath: path);
   }
 
-  /// Reads configuration file at [path] and add data to [_config] map.
+  /// Reads configuration file at [path] and set data to [_customConfig] map.
+  ///
+  /// If [path] is not passed, [kConfigFilePath] is used as default.
   Future<void> loadConfig({String path = kConfigFilePath}) async {
     try {
       if (!await isConfigFileAvailable(path: path)) {
@@ -51,7 +74,8 @@ class ConfigService {
 
       final data = await _fileService.readFileAsString(filePath: path);
 
-      _config = Config.fromJson(jsonDecode(data));
+      _customConfig = Config.fromJson(jsonDecode(data));
+      _hasCustomConfig = true;
     } on ConfigFileNotFoundException catch (e) {
       _log.warn(message: e.message);
     } on FormatException catch (_) {
@@ -59,5 +83,60 @@ class ConfigService {
     } catch (e) {
       _log.error(message: e.toString());
     }
+  }
+
+  /// Replaces the default configuration in [path] by custom configuration
+  /// available at [customConfig].
+  ///
+  /// If [hasCustomConfig] is false, returns [path] without modifications.
+  String replaceCustomPaths(String path) {
+    if (!hasCustomConfig) return path;
+
+    final customConfig = _customConfig.toJson();
+    String customPath = path;
+
+    for (var k in _defaultConfig.keys) {
+      if (customPath.contains(_defaultConfig[k])) {
+        customPath = customPath.replaceFirst(
+          _defaultConfig[k],
+          customConfig[k],
+        );
+        break;
+      }
+    }
+
+    return customPath;
+  }
+
+  /// Returns import path of [path].
+  String _getImportPath(String path) {
+    if (!path.startsWith('lib/')) return path;
+
+    return path.replaceFirst('lib/', '');
+  }
+
+  /// Returns import path relative to [path] of test helpers and mock services.
+  String _getRelativePathToHelpersAndMocks(
+    /// Relative path of file where test helpers and mocks will be imported.
+    String path,
+  ) {
+    _log.warn(message: 'path:$path testHelperPath:$testHelpersPath');
+    if (!path.startsWith('test/')) {
+      throw Exception(
+        'Any test file should be placed inside "test" folder on root of the project. Please, correct your test location.',
+      );
+    }
+
+    /// Remove unnecessary part of the path
+    String anyTestFilePath = path.replaceFirst('test/', '');
+    String helpersPath = testHelpersPath.replaceFirst('test/', '');
+
+    final anyTestFilePathLevels = anyTestFilePath.split('/');
+
+    for (var i = 0; i < anyTestFilePathLevels.length; i++) {
+      helpersPath = '../' + helpersPath;
+    }
+
+    return helpersPath;
   }
 }
