65661f1b742378caab769ef4854b3bf8741003ab
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2022-06-02 09:17:25 +0300

    add dialogs parameter to StackedApp annotation

---
+++ packages/stacked_core/lib/src/code_generation/dialogs/stacked_dialog.dart
@@ -0,0 +1,11 @@
+/// Described the Dialog functionality to generate in the app
+class StackedDialog {
+  /// Pass Dialogs types here.
+  ///
+  /// dialogs must extends [DialogBase]
+  final Type classType;
+
+  const StackedDialog({
+    required this.classType,
+  });
+}
