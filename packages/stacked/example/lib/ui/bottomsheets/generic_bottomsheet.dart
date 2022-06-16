5a3f73cfa5ad7fd0debf18a0f4ea440d563ced1c
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2022-06-16 13:54:25 +0300

    update stacked example with generated bottomsheets

---
+++ packages/stacked/example/lib/ui/bottomsheets/generic_bottomsheet.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class GenericBottomSheet extends StatelessWidget {
+  final SheetRequest request;
+  final Function(SheetResponse<GenericBottomSheetResponse>) completer;
+
+  const GenericBottomSheet({
+    Key? key,
+    required this.request,
+    required this.completer,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(25),
+      padding: EdgeInsets.all(25),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            request.title ?? 'Generic Bottom Sheet',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[900],
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            request.description ?? '',
+            style: TextStyle(color: Colors.grey),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              MaterialButton(
+                onPressed: () => completer(SheetResponse(
+                  confirmed: true,
+                  data: GenericBottomSheetResponse(message: 'SecondaryButton'),
+                )),
+                child: Text(
+                  request.secondaryButtonTitle ?? '',
+                  style: TextStyle(color: Theme.of(context).primaryColor),
+                ),
+              ),
+              TextButton(
+                onPressed: () => completer(SheetResponse(
+                  confirmed: true,
+                  data: GenericBottomSheetResponse(message: 'MainButton'),
+                )),
+                child: Text(
+                  request.mainButtonTitle ?? '',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Theme.of(context).primaryColor,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class GenericBottomSheetResponse {
+  const GenericBottomSheetResponse({
+    this.message = 'GenericBottomSheetResponse',
+  });
+
+  final String message;
+}
