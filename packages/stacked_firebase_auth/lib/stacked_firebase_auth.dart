698de26dedbf504edc00d9735e2ba5e308474a06
Fernando Ferrara <ferrarafer@gmail.com>
2022-06-23 20:20:52 -0300

    Expose FirebaseAuthException outside
    This is to avoid importing firebase_auth package only to use this
    exception.

--- packages/stacked_firebase_auth/lib/stacked_firebase_auth.dart
+++ packages/stacked_firebase_auth/lib/stacked_firebase_auth.dart
@@ -1,3 +1,5 @@
 library stacked_firebase_auth;
 
 export 'src/firebase_authentication_service.dart';
+export 'package:firebase_auth_platform_interface/firebase_auth_platform_interface.dart'
+    show FirebaseAuthException;
