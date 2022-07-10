6e64721d296659a1692da872b69945145d1d813e
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2022-07-10 21:46:12 +0300

    made a progress in the router class builder

--- packages/stacked_generator/lib/src/generators/router/generator/router_class_builder.dart
+++ packages/stacked_generator/lib/src/generators/router/generator/router_class_builder.dart
@@ -1,24 +1,19 @@
-import 'package:stacked_generator/src/generators/base_generator.dart';
+import 'package:code_builder/code_builder.dart';
 
 import '../../../../route_config_resolver.dart';
-import '../../../../utils.dart';
 
-class RouterClassBuilder with StringBufferUtils {
+class RouterClassBuilder {
   final List<RouteConfig> routes;
-  final String routesClassName;
   final String routerClassName;
-  RouterClassBuilder({
-    required this.routes,
-    required this.routesClassName,
-    required this.routerClassName,
-  });
+  const RouterClassBuilder(
+      {required this.routes, required this.routerClassName});
 
-  /// Example result
+  /// Example
   ///
   /// class routerClassName extends RouterBase {
-  ///      @override
-  ///      List<RouteDef> get routes => _routes;
-  ///      final _routes = <RouteDef>[
+  /// @override
+  /// List<RouteDef> get routes => _routes;
+  /// final _routes = <RouteDef>[
   ///
   ///
   /// RouteDef(RoutesClassName.loginView
@@ -39,64 +34,86 @@ class RouterClassBuilder with StringBufferUtils {
   /// var args = data.getArgs<HomeClassArguments>(
   /// orElse: ()=> HomeClassArguments(),);return MaterialPageRoute<dynamic>(builder: (context) =>   HomeClass(homeArg:args.homeArg), settings: data,);
   /// },};}
-  void addRouterClass() {
-    writeLine('\nclass $routerClassName extends RouterBase {');
+  Class BuildRouterClass() {
+    final routesDef = routes.map((route) =>
+        Reference('RouteDef', 'package:stacked/stacked.dart').newInstance([
+          Reference('Routes').property(route.name)
+        ], {
+          'page': Reference(
+            route.className,
+            // className should be a map its key is the class name and value is the import
+          )
+        }));
 
-    writeLine('''
-     @override
-     List<RouteDef> get routes => _routes;
-     final _routes = <RouteDef>[
-     ''');
-    for (var route in routes) {
-      _addRouteTemplates(route, routesClassName);
-    }
-    newLine();
-    write('];');
+    final routesField = Field((b) => b
+      ..name = '_routes'
+      ..modifier = FieldModifier.final$
+      ..assignment = literalList(
+              routesDef, Reference('RouteDef', 'package:stacked/stacked.dart'))
+          .code);
 
-    write(
-      '''
-       @override
-       Map<Type, StackedRouteFactory> get pagesMap => _pagesMap;
-        final _pagesMap = <Type, StackedRouteFactory>{
-        ''',
-      postlines: 2,
-    );
+    final routesGetterField = Method((b) => b
+      ..name = 'routes'
+      ..annotations.add(Reference('override'))
+      ..type = MethodType.getter
+      ..returns = TypeReference((b) => b
+        ..symbol = 'List'
+        ..types.add(TypeReference((b) => b
+          ..symbol = 'RouteDef'
+          ..url = 'package:stacked/stacked.dart'
+          ..isNullable = true)))
+      ..body = Reference('_routes').code);
 
-    for (var route in routes) {
-      _addRouteGeneratorFunction(route);
-    }
+    return Class((b) => b
+      ..name = routerClassName
+      ..extend = Reference('RouterBase', 'package:stacked/stacked.dart')
+      ..fields.add(routesField)
+      ..methods.add(routesGetterField));
 
-    write('};');
+    // write(
+    //   '''
+    //    @override
+    //    Map<Type, StackedRouteFactory> get pagesMap => _pagesMap;
+    //     final _pagesMap = <Type, StackedRouteFactory>{
+    //     ''',
+    //   postlines: 2,
+    // );
 
-    writeLine('}');
+    // for (var route in routes) {
+    //   _addRouteGeneratorFunction(route);
+    // }
+
+    // write('};');
+
+    // writeLine('}');
   }
 
   /// RouteDef(RoutesClassName._homeView
   /// ,page: HomeClass
   /// ),
-  void _addRouteTemplates(RouteConfig route, String routesClassName) {
-    write(
-      "RouteDef(${routesClassName}.${route.templateName}",
-      preLines: 1,
-      postlines: 1,
-    );
-    writeLine(",page: ${route.className}");
+  // void _addRouteTemplates(RouteConfig route) {
+  //   write(
+  //     "RouteDef(${routesClassName}.${route.templateName}",
+  //     preLines: 1,
+  //     postlines: 1,
+  //   );
+  //   writeLine(",page: ${route.className}");
 
-    if (route.children.isNotEmpty) {
-      writeLine(",generator: ${capitalize(route.name)}Router(),");
-    }
-    write('),');
-  }
+  //   if (route.children.isNotEmpty) {
+  //     writeLine(",generator: ${capitalize(route.name)}Router(),");
+  //   }
+  //   write('),');
+  // }
 
   /// HomeClass: (data) {
   /// var args = data.getArgs<HomeClassArguments>(
   /// orElse: ()=> HomeClassArguments(),);return MaterialPageRoute<dynamic>(builder: (context) =>   HomeClass(homeArg:args.homeArg), settings: data,);
   /// },
-  void _addRouteGeneratorFunction(RouteConfig route) {
-    writeLine('${route.className}: (data) {');
+  // void _addRouteGeneratorFunction(RouteConfig route) {
+  //   writeLine('${route.className}: (data) {');
 
-    write(route.registerRoutes());
+  //   write(route.registerRoutes());
 
-    write("},");
-  }
+  //   write("},");
+  // }
 }
