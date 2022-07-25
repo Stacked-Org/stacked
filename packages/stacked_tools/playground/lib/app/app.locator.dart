5e0b09a99a75c653e74069e34b2fe7da6adce5bc
Eimen2018 <nur.amuni26@gmail.com>
2022-07-25 14:42:09 +0300

    Updates dependencies

--- packages/stacked_tools/playground/lib/app/app.locator.dart
+++ packages/stacked_tools/playground/lib/app/app.locator.dart
@@ -6,13 +6,13 @@
 
 // ignore_for_file: public_member_api_docs
 
-import 'package:stacked/stacked.dart';
-import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_core/stacked_core.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 final locator = StackedLocator.instance;
 
-void setupLocator({String? environment, EnvironmentFilter? environmentFilter}) {
+Future<void> setupLocator(
+    {String? environment, EnvironmentFilter? environmentFilter}) async {
 // Register environments
   locator.registerEnvironment(
       environment: environment, environmentFilter: environmentFilter);
