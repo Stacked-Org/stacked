25523566920c037627d3c63973b07856a5c115b0
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2022-08-06 18:31:33 +0300

    fix analyzer warnings

--- packages/stacked/test/streamviewmodel_test.dart
+++ packages/stacked/test/streamviewmodel_test.dart
@@ -1,6 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:stacked/src/state_management/multiple_data_models.dart';
-import 'package:stacked/src/state_management/single_data_models.dart';
 import 'package:stacked/stacked.dart';
 
 Stream<int> numberStream(int dataBack,
