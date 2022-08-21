619b7d52a541f211989a3b933bd917cd84aa7939
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2022-08-21 14:03:23 +0300

    - Convert `FormViewModel` to a mixin - Rename `FormViewModel` to `FormStateHelper`

--- packages/stacked/lib/stacked.dart
+++ packages/stacked/lib/stacked.dart
@@ -3,7 +3,7 @@ library stacked;
 /// viewmodels state
 export 'src/state_management/base_view_models.dart';
 export 'src/state_management/index_tracking_viewmodel.dart';
-export 'src/state_management/form_viewmodel.dart';
+export 'src/state_management/helpers/form_state_helper.dart';
 
 export 'src/state_management/reactive_service_mixin.dart';
 export 'src/state_management/view_model_builder.dart';
