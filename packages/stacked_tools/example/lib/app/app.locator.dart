77c62ebf221c53fdda9562c940fd6add4c7b8115
Dane Mackier <dane.mackier@outlook.com>
2022-09-07 08:50:09 +0200

    Fixes example app

--- packages/stacked_tools/example/lib/app/app.locator.dart
+++ packages/stacked_tools/example/lib/app/app.locator.dart
@@ -4,10 +4,12 @@
 // StackedLocatorGenerator
 // **************************************************************************
 
-// ignore_for_file: public_member_api_docs
+// ignore_for_file: public_member_api_docs, implementation_imports, depend_on_referenced_packages
 
 import 'package:stacked_core/stacked_core.dart';
-import 'package:stacked_services/stacked_services.dart';
+import 'package:stacked_services/src/bottom_sheet/bottom_sheet_service.dart';
+import 'package:stacked_services/src/dialog/dialog_service.dart';
+import 'package:stacked_services/src/navigation/navigation_service.dart';
 
 final locator = StackedLocator.instance;
 
