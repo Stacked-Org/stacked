4950e3664a5b8c3e53d92cdd0f5cbf80334fbc74
Fernando Ferrara <ferrarafer@gmail.com>
2022-11-20 12:51:53 -0300

    Load config on create app command

--- packages/stacked_tools/lib/src/commands/create/create_app_command.dart
+++ packages/stacked_tools/lib/src/commands/create/create_app_command.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:args/command_runner.dart';
 import 'package:stacked_tools/src/locator.dart';
 import 'package:stacked_tools/src/services/colorized_log_service.dart';
+import 'package:stacked_tools/src/services/config_service.dart';
 import 'package:stacked_tools/src/services/process_service.dart';
 import 'package:stacked_tools/src/services/template_service.dart';
 import 'package:stacked_tools/src/templates/template_constants.dart';
@@ -21,6 +22,8 @@ class CreateAppCommand extends Command {
 
   @override
   Future<void> run() async {
+    await locator<ConfigService>().loadConfig();
+
     final appName = argResults!.rest.first;
 
     await _processService.runCreateApp(appName: appName);
