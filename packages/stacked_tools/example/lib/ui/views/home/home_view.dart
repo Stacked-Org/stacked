84296c91cd5276439ed8f2b727e3bfb0609cf4e3
Fernando Ferrara <ferrarafer@gmail.com>
2022-11-30 00:03:03 -0300

    Recreate example app with stacked_cli

--- packages/stacked_tools/example/lib/ui/views/home/home_view.dart
+++ packages/stacked_tools/example/lib/ui/views/home/home_view.dart
@@ -5,74 +5,76 @@ import 'package:example/ui/common/ui_helpers.dart';
 
 import 'home_viewmodel.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StackedView<HomeViewModel> {
   const HomeView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ViewModelBuilder<HomeViewModel>.reactive(
-      viewModelBuilder: () => HomeViewModel(),
-      builder: (context, model, child) => Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  verticalSpaceLarge,
-                  Column(
-                    children: [
-                      const Text(
-                        'Hello, STACKED!',
+  Widget builder(BuildContext context, HomeViewModel model, Widget? child) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                verticalSpaceLarge,
+                Column(
+                  children: [
+                    const Text(
+                      'Hello, STACKED!',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    verticalSpaceMedium,
+                    MaterialButton(
+                      color: Colors.black,
+                      onPressed: model.incrementCounter,
+                      child: Text(
+                        model.counterLabel,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    MaterialButton(
+                      color: kcDarkGreyColor,
+                      child: const Text(
+                        'Show Dialog',
                         style: TextStyle(
-                          fontSize: 35,
-                          fontWeight: FontWeight.w900,
+                          color: Colors.white,
                         ),
                       ),
-                      verticalSpaceMedium,
-                      MaterialButton(
-                        color: Colors.black,
-                        onPressed: model.incrementCounter,
-                        child: Text(
-                          model.counterLabel,
-                          style: const TextStyle(color: Colors.white),
+                      onPressed: model.showDialog,
+                    ),
+                    MaterialButton(
+                      color: kcDarkGreyColor,
+                      child: const Text(
+                        'Show Bottom Sheet',
+                        style: TextStyle(
+                          color: Colors.white,
                         ),
                       ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      MaterialButton(
-                        color: kcDarkGreyColor,
-                        child: const Text(
-                          'Show Dialog',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        onPressed: model.showDialog,
-                      ),
-                      MaterialButton(
-                        color: kcDarkGreyColor,
-                        child: const Text(
-                          'Show Bottom Sheet',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        onPressed: model.showBottomSheet,
-                      ),
-                    ],
-                  )
-                ],
-              ),
+                      onPressed: model.showBottomSheet,
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  @override
+  HomeViewModel modelBuilder(BuildContext context) {
+    return HomeViewModel();
   }
 }
--- packages/stacked_tools/example/test/helpers/test_helpers.dart
+++ packages/stacked_tools/example/test/helpers/test_helpers.dart
@@ -1,8 +1,7 @@
-import 'package:example/app/app.locator.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:example/app/app.locator.dart';
 import 'package:stacked_services/stacked_services.dart';
-
 // @stacked-import
 
 import 'test_helpers.mocks.dart';
