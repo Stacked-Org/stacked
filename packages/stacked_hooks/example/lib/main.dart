64dc4afec9158e1ed443314115c428a76b0f70a8
Fernando Ferrara <ferrarafer@gmail.com>
2022-12-15 23:47:39 -0300

    Update example app

--- packages/stacked_hooks/example/lib/main.dart
+++ packages/stacked_hooks/example/lib/main.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:stacked_hooks_example/ui/common/app_colors.dart';
+import 'package:stacked_hooks_example/ui/setup/setup_bottom_sheet_ui.dart';
+import 'package:stacked_hooks_example/ui/setup/setup_dialog_ui.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-import 'ui/views/hook_viewmodel_widget/hook_viewmodel_widget_example.dart';
+import 'app/app.locator.dart';
+import 'app/app.router.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  setupLocator();
+  setupDialogUi();
+  setupBottomSheetUi();
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: HookViewModelWidgetExample(),
+      theme: Theme.of(context).copyWith(
+        primaryColor: kcBackgroundColor,
+        focusColor: kcPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+            ),
+      ),
+      initialRoute: Routes.startupView,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      navigatorKey: StackedService.navigatorKey,
     );
   }
 }
