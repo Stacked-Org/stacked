c0b7bfc6841a8a33e1d2e4db2bbd32e49d2c5292
Dane Mackier <dane.mackier@outlook.com>
2023-01-08 13:56:02 +0200

    [hooks] Deprecates the HookViewModelWidget

--- packages/stacked_hooks/lib/src/_stacked_hook_view.dart
+++ packages/stacked_hooks/lib/src/_stacked_hook_view.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 
+@Deprecated(
+    'This widget will be removed by March 2023, please use the StackedHookView instead')
+abstract class HookViewModelWidget<T> extends StackedHookView<T> {
+  const HookViewModelWidget({Key? key, super.reactive = true});
+}
+
 /// An implementation of the StackedView that allows you to use Hooks in the
 /// build
 abstract class StackedHookView<T> extends HookWidget {
