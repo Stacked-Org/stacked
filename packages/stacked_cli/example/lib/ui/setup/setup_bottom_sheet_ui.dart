72d267b1219e9c28197ef5413250c6888355be1e
Fernando Ferrara <ferrarafer@gmail.com>
2023-01-13 10:40:26 -0300

    Use SheetBuilder from BottomSheetService

--- packages/stacked_cli/example/lib/ui/setup/setup_bottom_sheet_ui.dart
+++ packages/stacked_cli/example/lib/ui/setup/setup_bottom_sheet_ui.dart
@@ -1,22 +1,15 @@
-import 'package:flutter/material.dart';
 import 'package:example/app/app.locator.dart';
 import 'package:example/enums/bottom_sheet_type.dart';
 import 'package:example/ui/bottom_sheets/notice/notice_sheet.dart';
 import 'package:stacked_services/stacked_services.dart';
 // @stacked-import
 
-typedef BottomSheetBuilder = Widget Function(
-  BuildContext context,
-  SheetRequest<dynamic> sheetRequest,
-  void Function(SheetResponse<dynamic> sheetResponse) completer,
-);
-
 void setupBottomSheetUi() {
   final bottomSheetService = locator<BottomSheetService>();
 
-  final Map<dynamic, BottomSheetBuilder> builders = {
-    BottomSheetType.notice: (context, sheetRequest, completer) =>
-        NoticeSheet(completer: completer, request: sheetRequest),
+  final Map<dynamic, SheetBuilder> builders = {
+    BottomSheetType.notice: (context, request, completer) =>
+        NoticeSheet(completer: completer, request: request),
     // @stacked-bottom-sheet-builder
   };
 
