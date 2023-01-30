b213476b31e72eb8fa616bf286040d9e5ce202df
Fernando Ferrara <ferrarafer@gmail.com>
2023-01-30 12:49:12 -0300

    Update example app

---
+++ packages/stacked_cli/example/test/viewmodels/info_alert_dialog_model_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:example/app/app.locator.dart';
+
+import '../helpers/test_helpers.dart';
+
+void main() {
+  group('InfoAlertDialogModel Tests -', () {
+    setUp(() => registerServices());
+    tearDown(() => locator.reset());
+  });
+}
