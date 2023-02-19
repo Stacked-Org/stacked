9b3e76f3743081fc2952bcdb4d267243a46e8b74
Fernando Ferrara <ferrarafer@gmail.com>
2023-02-19 16:49:17 -0300

    Avoid running pub command if has last version

--- packages/stacked_cli/lib/src/commands/update/update_command.dart
+++ packages/stacked_cli/lib/src/commands/update/update_command.dart
@@ -4,10 +4,12 @@ import 'package:args/command_runner.dart';
 import 'package:stacked_cli/src/locator.dart';
 import 'package:stacked_cli/src/services/analytics_service.dart';
 import 'package:stacked_cli/src/services/process_service.dart';
+import 'package:stacked_cli/src/services/pub_service.dart';
 
 class UpdateCommand extends Command {
   final _analyticsService = locator<AnalyticsService>();
   final _processService = locator<ProcessService>();
+  final _pubService = locator<PubService>();
 
   @override
   String get description => '''Updates stacked_cli to latest version''';
@@ -18,6 +20,8 @@ class UpdateCommand extends Command {
   @override
   Future<void> run() async {
     unawaited(_analyticsService.updateCliEvent());
+    if (await _pubService.hasLatestVersion()) return;
+
     await _processService.runPubGlobalActivate();
   }
 }
