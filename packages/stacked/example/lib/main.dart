f3d3a89e68b3cdd989ae6921a6ecc16419017852
Dane Mackier <dane.mackier@outlook.com>
2023-03-05 14:57:19 +0200

    [generator] Fixes the reference bug with enum values

--- packages/stacked/example/lib/main.dart
+++ packages/stacked/example/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 import 'app/app.locator.dart';
-import 'app/app.router.dart';
 
 void main() {
   setupExampleLocator(environment: Environment.dev);
@@ -18,7 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       navigatorKey: StackedService.navigatorKey,
-      onGenerateRoute: StackedRouter().onGenerateRoute,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
