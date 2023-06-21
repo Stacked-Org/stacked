012891fdc1c5b5bb842a170a4c7e85fd50753af7
Fernando Ferrara <ferrarafer@gmail.com>
2023-06-21 09:04:35 -0300

    chore: Update router example (#967)
    * Add RouterServiceInterface to Stacked package
    
    * Add setCurrentWebPageIndex method to IndexTrackingViewModel
    
    * Update cupertino_icons on example apps
    
    * Sort routes to match BottomNavigation items order
    
    * Call setCurrentWebPageIndex on Class Constructor
    
    Allows to set the index of the Bottom Navigation Bar using the current
    route at Web Platform when app starts from scratch which means there is
    no state for the currentIndex property to track the selected button.
    
    * Update router_example to test InitializableSingleton
    
    Update example
    
    Squash

--- example/router_example/lib/app/app.dart
+++ example/router_example/lib/app/app.dart
@@ -1,6 +1,8 @@
 import 'package:example/services/epoch_service.dart';
 import 'package:example/services/factory_service.dart';
 import 'package:example/services/information_service.dart';
+import 'package:example/services/native_actions_service.dart';
+import 'package:example/services/shared_preferences_service.dart';
 import 'package:example/ui/bottom_nav/bottom_nav_example.dart';
 import 'package:example/ui/bottom_nav/favorites/favorites_view.dart';
 import 'package:example/ui/bottom_nav/favorites/favorites_viewmodel.dart';
@@ -8,6 +10,7 @@ import 'package:example/ui/bottom_nav/history/history_view.dart';
 import 'package:example/ui/bottom_nav/history/history_viewmodel.dart';
 import 'package:example/ui/bottom_nav/profile/profile_view.dart';
 import 'package:example/ui/bottomsheets/generic_bottomsheet.dart';
+import 'package:example/ui/dialogs/basic_dialog.dart';
 import 'package:example/ui/form/example_form_view.dart';
 import 'package:example/ui/home/home_view.dart';
 import 'package:example/ui/multiple_futures_example/multiple_futures_example_view.dart';
@@ -18,7 +21,6 @@ import 'package:stacked_services/stacked_services.dart';
 import 'package:stacked_shared/stacked_shared.dart';
 import 'package:stacked_themes/stacked_themes.dart';
 
-import '../ui/dialogs/basic_dialog.dart';
 import 'custom_route_transition.dart';
 
 @StackedApp(
@@ -57,17 +59,14 @@ import 'custom_route_transition.dart';
     // Lazy singletons
     LazySingleton(classType: DialogService),
     LazySingleton(classType: BottomSheetService),
-    // LazySingleton(
-    //   classType: InformationService,
-    //   dispose: disposeInformationService,
-    // ),
     LazySingleton(
       classType: RouterService,
     ),
     LazySingleton(
-        classType: NavigationService,
-        environments: {Environment.dev},
-        instanceName: 'instance1'),
+      classType: NavigationService,
+      environments: {Environment.dev},
+      instanceName: 'instance1',
+    ),
     LazySingleton(classType: EpochService),
     LazySingleton(
       classType: ThemeService,
@@ -76,9 +75,20 @@ import 'custom_route_transition.dart';
     LazySingleton(classType: InformationService),
     LazySingleton(classType: InformationService, instanceName: 'infoInstance1'),
     FactoryWithParam(classType: FactoryService),
-    // singletons
+    // Singletons
     Singleton(classType: HistoryViewModel),
     Singleton(classType: FavoritesViewModel),
+
+    InitializableSingleton(classType: SharedPreferencesService),
+    // Presolve(
+    //   classType: SharedPreferencesService,
+    //   presolveUsing: SharedPreferencesService.getInstance,
+    // ),
+    InitializableSingleton(classType: NativeActionsService),
+    // Presolve(
+    //   classType: NativeActionsService,
+    //   presolveUsing: NativeActionsService.getInstance,
+    // ),
   ],
   logger: StackedLogger(),
   locatorName: 'exampleLocator',
--- example/router_example/lib/ui/home/home_view.dart
+++ example/router_example/lib/ui/home/home_view.dart
@@ -1,10 +1,10 @@
 import 'package:example/datamodels/clashable_one.dart';
 import 'package:example/datamodels/home_type.dart';
+import 'package:example/ui/smart_widgets/widget_one/widget_one.dart';
+import 'package:example/ui/smart_widgets/widget_two/widget_two.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
-import '../smart_widgets/widget_one/widget_one.dart';
-import '../smart_widgets/widget_two/widget_two.dart';
 import 'home_viewmodel.dart';
 
 class HomeView extends StatelessWidget {
@@ -18,10 +18,7 @@ class HomeView extends StatelessWidget {
     this.title = 'hello',
     this.isLoggedIn = false,
     this.clashableGetter,
-    this.homeTypes = const [
-      HomeType.apartment,
-      HomeType.house,
-    ],
+    this.homeTypes = const [HomeType.apartment, HomeType.house],
   }) : super(key: key);
 
   @override
@@ -31,23 +28,53 @@ class HomeView extends StatelessWidget {
       onViewModelReady: (viewModel) => viewModel.initialise(),
       builder: (context, viewModel, child) => Scaffold(
         body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(viewModel.title),
-              if (viewModel.hasMessage) Text(viewModel.modelMessage!),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const <Widget>[
-                  WidgetOne(),
-                  SizedBox(width: 50),
-                  WidgetTwo(id: 2),
-                ],
-              ),
-              const SizedBox(height: 20),
-              if (title != null) Text(title!),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(12, 20, 12, 40),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const Expanded(child: SizedBox.shrink()),
+                Text(viewModel.title),
+                if (viewModel.hasMessage) Text(viewModel.modelMessage!),
+                const SizedBox(height: 20),
+                const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    WidgetOne(),
+                    SizedBox(width: 50),
+                    WidgetTwo(id: 2),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                if (title != null) Text(title!),
+                const SizedBox(height: 20),
+                MaterialButton(
+                  onPressed: viewModel.isUserLoggedIn
+                      ? viewModel.signOutUser
+                      : viewModel.signInUser,
+                  color: Colors.blue.shade400,
+                  height: 60,
+                  minWidth: 150,
+                  child: viewModel.isBusy
+                      ? const CircularProgressIndicator(
+                          color: Colors.white,
+                        )
+                      : Text(
+                          viewModel.isUserLoggedIn ? 'Sign Out' : 'Sign In',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                ),
+                const Expanded(child: SizedBox.shrink()),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Operative System: ${viewModel.operativeSystem}'),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Device ID: ${viewModel.deviceId}'),
+                ),
+              ],
+            ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
